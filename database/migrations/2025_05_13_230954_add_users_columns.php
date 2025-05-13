a0eb0e2f7e994dfa23a98e0071d25e0a56619769
Thomas Mauro <thom_mauro@outlook.com>
2025-05-13 20:14:23 -0300

    columns added to users table

---
+++ database/migrations/2025_05_13_230954_add_users_columns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('nome_completo', 100);
+            $table->string('cpf', 11);
+            $table->string('logradouro', 256);
+            $table->string('numero_logradouro', 10);
+            $table->string('bairro', 64);
+            $table->string('municipio', 64);
+            $table->string('uf', 2);
+            $table->string('cep', 8);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
