8a2f71a939ff5938293ada1d53eb86126d3637f2
Thomas Mauro <thom_mauro@outlook.com>
2025-06-21 12:22:14 -0300

    added unique:emails in request

--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -23,7 +23,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'full_name' => 'required',
-            'email' => 'required',
+            'email' => ['required', 'unique:users,email'],
             'password' => 'required',
         ];
     }
